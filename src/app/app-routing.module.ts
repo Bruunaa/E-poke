0edfdba8db09f39e0d64756158fdbdf6cab7123c
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-05-19 12:58:52 -0300

    Criação do componente Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path:'navbar', component: NavbarComponent},
 ];
 
 @NgModule({
