24f50bfcc2bd33aa7f9b4f3034558dc0d89ea719
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-05-23 22:29:03 -0300

    Adição da página de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ContatoComponent } from './components/contato/contato.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -17,6 +18,7 @@ const routes: Routes = [
   {path: 'feedback', component:FeedbackComponent},
   {path: 'produtos', component:ProdutosComponent},
   {path: 'servicos', component: ServicosComponent},
+  {path: 'contato', component: ContatoComponent},
   {path: '**', component: NotFoundComponent},
 
 ];
