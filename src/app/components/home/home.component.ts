c05ea78f704bfac86c93f8f9ff45a53fa522fecb
bianca-goncalvess <bianca.adm01@gmail.com>
2022-05-24 02:05:08 -0300

    Implementação de imagem da Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,12 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    imagem: string = "https://gamersrd.com/wp-content/uploads/2019/11/Pokemon-Top-5-juegos.jpg";
-    imagem2: string = "https://gamersrd.com/wp-content/uploads/2019/11/Pokemon-Top-5-juegos.jpg";
-    imagem3: string = "https://gamersrd.com/wp-content/uploads/2019/11/Pokemon-Top-5-juegos.jpg";
-    imagem4: string = "https://static6.minhalojanouol.com.br/pokestoprodutos/produto/20220201112202_5399994601_D.jpg";
-
-  
+  imagem: string = "https://jazwaresv2.s3.amazonaws.com/media/ATG002074_2022_POK_JazBanner.jpg";
+  imagem2: string = "https://alltimelines.com/wp-content/uploads/2019/10/pokemongames-banner.jpg";
+  imagem3: string = "https://gamersrd.com/wp-content/uploads/2019/11/Pokemon-Top-5-juegos.jpg";
+  imagem4: string = "https://3.bp.blogspot.com/--__pYNgV-ms/VY9fFjhNjsI/AAAAAAAAb38/oUj3QSujBZQ/s1600/Dream_Pok%25C3%25A9_Ball_Sprite.png";
+  imagem5: string = "https://1.bp.blogspot.com/-956bABkamzs/VydUlRHGesI/AAAAAAAApxU/cgzKTsqrgakWyodfRj6ZtDuVfsCZoGeUgCK4B/s1600/Dream_Water_Stone_Sprite.png";
+  imagem6: string = "https://3.bp.blogspot.com/-FN3ndWkELcI/V0HfC6ooB-I/AAAAAAAAq8w/cqByVsOJUSgQs97PbB5NfdknWUGrRaztwCK4B/s400/pokebola_timer.gif";
+  imagem7: string = "https://3.bp.blogspot.com/-hePneIVAcQY/VydUj6zCsmI/AAAAAAAApwo/8REOLMmtSucfXr7O7DhFoAcPS-TnoPAOwCK4B/s1600/Dream_Shiny_Stone_Sprite.png";
   
   constructor() { }
 
