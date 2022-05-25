8d5acc31698a73aae5a4e599db453fdbefe8cc03
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-05-24 21:58:04 -0300

    Reparo no Carrossel da Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -17,6 +16,7 @@ import { ServicosComponent } from './components/servicos/servicos.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ContatoComponent } from './components/contato/contato.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
@@ -33,7 +33,8 @@ import { ContatoComponent } from './components/contato/contato.component';
     ProdutosComponent,
     ServicoComponent,
     ServicosComponent,
-    ContatoComponent
+    ContatoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
